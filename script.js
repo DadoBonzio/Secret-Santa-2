c41b10281dafe7400cfe4c3af4df6c1622002d1b
DadoBonzio <106774451+DadoBonzio@users.noreply.github.com>
2024-10-14 18:12:00 +0200

    My Commit

--- script.js
+++ script.js
@@ -10,7 +10,11 @@ const app = Vue.createApp({
     methods: {
 
         addPerson() {
+<<<<<<< HEAD
             if(this.name == "" || this.email == "") {
+=======
+            if (this.name == "" || this.email == "") {
+>>>>>>> 7a86233 (My Commit)
                 alert("Please enter name and email");
                 return;
             }
@@ -21,8 +25,14 @@ const app = Vue.createApp({
         removePerson(i) {
             this.names.splice(i, 1);
         },
+<<<<<<< HEAD
                 
         sendEmails() {
+=======
+
+        sendEmails() {
+            this.validateMails();
+>>>>>>> 7a86233 (My Commit)
             let arr = JSON.parse(JSON.stringify(this.names));
             for (let t = 0; t < new Date().getMilliseconds() % 5; t++) {
                 for (let i = arr.length - 1; i > 0; i--) {
@@ -46,6 +56,18 @@ const app = Vue.createApp({
                 emailjs.send("service_n05ddqf", "template_e33dzag", params);
             })
             window.alert("Emails sent, merry Christmas!")
+<<<<<<< HEAD
+=======
+        },
+        validateMails() {
+            let arr = JSON.parse(JSON.stringify(this.names));
+            for (let i = 0; i < arr.length; i++) {
+                if (!arr[i].email.includes("@")) {
+                    arr[i].email = arr[i].email + "@gmail.com"
+                }
+            }
+            this.names = arr
+>>>>>>> 7a86233 (My Commit)
         }
     }
 })
